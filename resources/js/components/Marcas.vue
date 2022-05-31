a15de468f893fd964e61f6cfa0a57d32943a45de
Felipe118 <lipebezerraa@gmail.com>
2022-05-30 23:35:17 -0300

    feat: criando o componente de marcas

---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,90 @@
+<template>
+   <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Busca de marcas</div>
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col mb-3">
+                            <input-container-component 
+                                titulo="ID" 
+                                id="inputId"
+                                id-help="IdHelp"
+                                texto-ajuda="Opcional. Informe o ID da marca"
+                            >
+                             <input type="number" class="form-control" id="InputId" aria-describedby="idHelp" placeholder="ID">
+                            </input-container-component>
+
+                           
+                           
+                           
+                        </div>
+                        <div class=" col mb-3">
+                          <input-container-component 
+                                titulo="Nome da marca" 
+                                id="inputNome"
+                                id-help="nomeHelp"
+                                texto-ajuda="Opcional. Informe o nome da marca"
+                            >
+                             <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                            </input-container-component>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
+                </div>
+            </div>
+            <!--Fim card de busca-->
+
+            <!--Inicio card de listagem de marca-->
+                <div class="card mb-5">
+                <div class="card-header">Relação de marcas</div>
+
+                <div class="card-body">
+                   <table class="table table-hover">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-primary btn-sm float-end">Adicionar</button>
+                </div>
+            </div>
+            <!--Fim card de listagem de marca-->
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+   
+    
+</script>
